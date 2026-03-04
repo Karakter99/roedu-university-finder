0064f353443f825230abb22f0b13684963768182
Yakup Akyniyazov <yakupakyniyazov@192.168.1.135>
2026-03-04 16:08:00 +0200

    Deploy in Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // <--- THIS IS CRITICAL FOR FIREBASE
   images: {
     unoptimized: true, // <--- Required for 'next/image' in static export
   },
